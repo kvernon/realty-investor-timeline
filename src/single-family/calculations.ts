a108ec8b817febbb1cd149f6c0bda6ece7cb6878
Kelly Vernon <kvernon@cubedelement.com>
2021-09-16 14:12:38 -0500

    feat: piti added

--- src/single-family/calculations.ts
+++ src/single-family/calculations.ts
@@ -6,6 +6,38 @@
  * @param loanRatePercent
  * @param yearOnLoan
  */
+export function getMonthlyPrincipalInterestTaxInterest(
+  purchasePrice: number,
+  percentDown: number,
+  closingCostPercent: number,
+  loanRatePercent: number,
+  yearOnLoan = 30
+): number {
+  const avgTexasPropertyTaxPercent = 2.19;
+  const annualTaxes: number =
+    (purchasePrice * avgTexasPropertyTaxPercent) / 100; //?
+
+  const fakeRateCalculation = 0.0053097345132743;
+  const annualInsurance: number = purchasePrice * fakeRateCalculation; //?
+
+  const monthlyMortgage = getMonthlyMortgage(
+    purchasePrice,
+    percentDown,
+    closingCostPercent,
+    loanRatePercent,
+    yearOnLoan
+  );
+  return monthlyMortgage + annualTaxes / 12 + annualInsurance / 12; //?
+}
+
+/**
+ *
+ * @param purchasePrice
+ * @param percentDown
+ * @param closingCostPercent
+ * @param loanRatePercent
+ * @param yearOnLoan
+ */
 export function getMonthlyMortgage(
   purchasePrice: number,
   percentDown: number,
@@ -19,15 +51,5 @@ export function getMonthlyMortgage(
   const i = loanRatePercent / 100 / 12; //?
   const n = yearOnLoan * 12; //?
 
-  const avgTexasPropertyTaxPercent = 2.19;
-  const annualTaxes: number =
-    (purchasePrice * avgTexasPropertyTaxPercent) / 100; //?
-
-  const fakeRateCalculation = 0.0053097345132743;
-  const annualInsurance: number = purchasePrice * fakeRateCalculation; //?
-
-  const monthlyMortgage =
-    (p * i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
-
-  return monthlyMortgage + annualTaxes / 12 + annualInsurance / 12; //?
+  return (p * i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
 }
--- tests/single-family/calculations.spec.ts
+++ tests/single-family/calculations.spec.ts
@@ -1,10 +1,28 @@
-import { getMonthlyMortgage } from "../../src/single-family/calculations";
+import {
+  getMonthlyPrincipalInterestTaxInterest,
+  getMonthlyMortgage,
+} from "../../src/single-family/calculations";
 
 describe("calculations unit tests", () => {
   describe("and getMonthlyMortgage", () => {
     describe("and success", () => {
       test("and values", () => {
         const monthlyMortgage = getMonthlyMortgage(205000, 25, 6, 4.24);
+
+        expect(monthlyMortgage).toBeGreaterThanOrEqual(755);
+        expect(monthlyMortgage).toBeLessThanOrEqual(760);
+      });
+    });
+  });
+  describe("and getMonthlyPrincipalInterestTaxInterest", () => {
+    describe("and success", () => {
+      test("and values", () => {
+        const monthlyMortgage = getMonthlyPrincipalInterestTaxInterest(
+          205000,
+          25,
+          6,
+          4.24
+        );
 
         expect(monthlyMortgage).toBeGreaterThanOrEqual(1220);
         expect(monthlyMortgage).toBeLessThanOrEqual(1225);
