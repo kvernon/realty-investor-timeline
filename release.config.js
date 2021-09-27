8fab8dfd40cc67d91caa73c32289393daa97ee47
Kelly Vernon <kvernon@cubedelement.com>
2021-09-27 08:16:01 -0500

    fix: updating to publish; removed github release

--- release.config.js
+++ release.config.js
@@ -2,9 +2,14 @@ module.exports = {
   'branches': ['main'],
   'plugins': [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/github',
     ['@semantic-release/release-notes-generator'],
-    ['@semantic-release/npm']
-  ],
-  'debug': true
+    ['@semantic-release/changelog', {
+      'changelogFile': 'docs/CHANGELOG.md'
+    }],
+    ['@semantic-release/npm'],
+    ['@semantic-release/git', {
+      'assets': ['docs', 'package.json', 'package-lock.json'],
+      'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }]
+  ]
 };
